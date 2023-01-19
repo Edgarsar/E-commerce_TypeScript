094387c79f470fabef1ea5c90ad043e2aff5e710
Edgar <edgarsargsyan@hotmail.com>
2023-01-19 16:08:39 -0600

    Define a types for the Category and CategoryItem

--- src/store/categories/category.types.ts
+++ src/store/categories/category.types.ts
@@ -5,3 +5,17 @@ export enum CATEGORIES_ACTION_TYPE {
   FETCH_CATEGORIES_SUCCES = "category/FETCH_CATEGORIES_SUCCES",
   FETCH_CATEGORIES_FAILED = "category/FETCH_CATEGORIES_FAILED",
 };
+
+
+export type CategoryItem = {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number
+};
+
+export type Category = {
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[];
+};

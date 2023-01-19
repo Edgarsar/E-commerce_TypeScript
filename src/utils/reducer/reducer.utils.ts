7684e1eb0968acc35f22ff198a394d1a49d3a7b9
Edgar <edgarsargsyan@hotmail.com>
2023-01-19 16:53:38 -0600

    Fix a bug

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,7 +1,7 @@
 
 import { AnyAction } from "redux";
 
-export type ActionWithPayload<P, T> = {
+export type ActionWithPayload<T, P> = {
   type: T;
   payload: P;
 };
@@ -10,12 +10,16 @@ export type Action<T> = {
   type: T;
 };
 
-export function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>;
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
 
-export function createAction<T extends string>(type: T, payload: void): Action<T>;
-
+export function createAction<T extends string>(
+  type: T,
+  payload: void
+): Action<T>;
 
 export function createAction<T extends string, P>(type: T, payload: P) {
   return { type, payload };
-};
-
+}

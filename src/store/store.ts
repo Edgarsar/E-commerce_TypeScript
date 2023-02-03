f2f465400d3b33829602286ae91333c0d6870794
Edgar <edgarsargsyan@hotmail.com>
2023-02-02 20:38:22 -0600

    Finished up typing store file

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,10 @@
-import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { compose, legacy_createStore as createStore, applyMiddleware, Middleware } from 'redux';
 
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 
-import { logger } from "redux-logger";
+import {logger }from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 // import thunk from 'redux-thunk';
@@ -13,24 +13,30 @@ import createSagaMiddleware from '@redux-saga/core';
 import { rootSaga } from './root-saga';
 
 declare global {
-  interface window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
 }
 
-
 const sagaMiddleware = createSagaMiddleware();
 
-const middleware = [process.env.NODE_ENV !== "production" && logger, sagaMiddleware].filter(Boolean);
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  sagaMiddleware,
+].filter((middleware): middleware is Middleware => Boolean(middleware));
+
 
 export type RootState = ReturnType<typeof rootReducer>
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["cart"]
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
 };
 
+const persistConfig: ExtendedPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
@@ -38,7 +44,7 @@ const composedEnhancer =
   (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-const composedEnhancers = composedEnhancer(applyMiddleware(...middleware));
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 

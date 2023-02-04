5ced7499f4f0eea104b3408c071af26f151f2097
Edgar <edgarsargsyan@hotmail.com>
2023-02-03 20:00:14 -0600

    Typing out cart-icon component

--- src/components/cart-icon/cart-icon.styles.tsx
+++ src/components/cart-icon/cart-icon.styles.tsx
@@ -1,11 +1,10 @@
-import styled from 'styled-components';
-import { ReactComponent as ShoppingSvg } from '../../assets/shopping-bag.svg';
+import styled from "styled-components";
+import { ReactComponent as ShoppingSvg } from "../../assets/shopping-bag.svg";
 
 export const ShoppingIcon = styled(ShoppingSvg)`
-    width: 24px;
-    height: 24px;
+  width: 24px;
+  height: 24px;
 `;
-
 
 export const CartIconContainer = styled.div`
   width: 45px;
@@ -18,10 +17,8 @@ export const CartIconContainer = styled.div`
 `;
 
 export const ItemCount = styled.span`
-    position: absolute;
-    font-size: 10px;
-    font-weight: bold;
-    bottom: 12px;
+  position: absolute;
+  font-size: 10px;
+  font-weight: bold;
+  bottom: 12px;
 `;
-
-

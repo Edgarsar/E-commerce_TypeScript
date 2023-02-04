31cd3835acb565cc8bd816da43d9f022aeb3e1e6
Edgar <edgarsargsyan@hotmail.com>
2023-02-03 19:11:45 -0600

    Type out Button component

--- src/components/button/button.styles.tsx
+++ src/components/button/button.styles.tsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { SpinnerContainer } from '../spinner/spinner.styles';
+import { SpinnerContainer } from "../spinner/spinner.styles";
 
 export const BaseButton = styled.button`
   min-width: 165px;
@@ -13,7 +13,7 @@ export const BaseButton = styled.button`
   background-color: black;
   color: white;
   text-transform: uppercase;
-  font-family: 'Open Sans Condensed';
+  font-family: "Open Sans Condensed";
   font-weight: bolder;
   border: none;
   cursor: pointer;
@@ -31,7 +31,7 @@ export const BaseButton = styled.button`
 export const GoogleSignInButton = styled(BaseButton)`
   background-color: #4285f4;
   color: white;
-  
+
   &:hover {
     background-color: #357ae8;
     border: none;
@@ -51,5 +51,5 @@ export const InvertedButton = styled(BaseButton)`
 
 export const ButtonSpinner = styled(SpinnerContainer)`
   width: 30px;
-  height :30px;
+  height: 30px;
 `;

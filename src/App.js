f8c5a00c433790bafb0db347f457c88ecebca7d8
Edgar <edgarsargsyan@hotmail.com>
2023-02-10 20:16:45 -0600

    Adding GlobalStyle component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { useDispatch } from "react-redux"
 import { Routes, Route } from 'react-router-dom';
 import Spinner from "./components/spinner/spinner.component";
 import { checkUserSession } from "./store/user/user.action";
+import { GlobalStyle } from "./global.styles";
 
 const Home = lazy(() => import('./routes/home/home.component'));
 const Authentication = lazy(() => import('./routes/authentication/authentication'));
 const Shop = lazy(() => import('./routes/shop/shop.component'));
 const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
 const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+
 
 
 const App = () => {
@@ -20,6 +22,7 @@ const App = () => {
 
   return (
     <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
       <Routes>
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />} />
